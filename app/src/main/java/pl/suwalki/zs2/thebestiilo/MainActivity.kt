bb5b029872bc77da21657573fb881483c789cf37
kapiziak <nznajomypl@gmail.com>
2019-03-13 22:32:51 +0100

    Testing 1

--- app/src/main/java/pl/suwalki/zs2/thebestiilo/MainActivity.kt
+++ app/src/main/java/pl/suwalki/zs2/thebestiilo/MainActivity.kt
@@ -27,7 +27,7 @@ import com.google.android.gms.tasks.OnSuccessListener
 
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { // o
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
